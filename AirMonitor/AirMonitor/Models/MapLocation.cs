2bbeb2ee42b69cd8904bbd1e027eff4cb08abcf8
Paweł Wójsik <pablo8899@interia.pl>
2020-07-03 21:04:49 +0200

    Lab5

---
+++ AirMonitor/AirMonitor/Models/MapLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms.Maps;
+
+namespace AirMonitor.Models
+{
+    public class MapLocation
+    {
+        public string Address { get; set; }
+        public Position Position { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ AirMonitor/AirMonitor/Views/MapPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using AirMonitor.ViewModels;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace AirMonitor.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        private HomeViewModel _viewModel => BindingContext as HomeViewModel;
+        public MapPage()
+        {
+            InitializeComponent();
+            BindingContext = new HomeViewModel(Navigation);
+            InitializeMap();
+        }
+
+        private async void InitializeMap()
+        {
+            var location = await _viewModel.GetLocation();
+
+            if (location == null)
+                return;
+
+            Map.IsShowingUser = true;
+
+            Map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude,location.Longitude), Distance.FromKilometers(1)));
+        }
+
+        private void Pin_InfoWindowClicked(object sender, Xamarin.Forms.Maps.PinClickedEventArgs e)
+        {
+            if(sender is Pin pin)
+            _viewModel?.MapPinTappedCommand.Execute(pin.Address);
+        }
+    }
+}
